b51338efb47d9737eaba59c8d8763d74599ec6d2
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-26 13:22:16 +0100

    Brand. Top Color and Top Spped

--- src/index.js
+++ src/index.js
@@ -27,11 +27,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import cars from "./practice";
 
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = honda;
+
 ReactDOM.render(
   <table>
     <tr>
       <th>Brand</th>
       <th>Top Speed</th>
+      <th>Top Colour</th>
     </tr>
     <tr>
       <td>{tesla.model}</td>
